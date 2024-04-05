3b77448f1f731395552b50bec83a70e13ce2accb
arief sangga <arief.heaven@gmail.com>
2024-04-05 12:37:43 +0700

    update uuid

---
+++ database/migrations/2024_04_05_114940_add_uuid_to_akademiks.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\Akademik;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Ramsey\Uuid\Uuid;
+
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('akademiks', function (Blueprint $table) {
+            //$table->uuid('uuid')->after('id')->nullable();
+
+            // Generate UUID for existing records
+            $akademik = Akademik::all();
+            foreach ($akademik as $akademik) {
+                $akademik->uuid = Uuid::uuid4()->toString();
+                $akademik->save();
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('akademiks', function (Blueprint $table) {
+            //
+        });
+    }
+};
