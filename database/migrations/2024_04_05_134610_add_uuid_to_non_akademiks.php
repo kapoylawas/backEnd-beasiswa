aedbf3bc8db49985e6e31cb9915beac4c289a967
arief sangga <arief.heaven@gmail.com>
2024-04-05 13:53:22 +0700

    update migrate non akademiks

---
+++ database/migrations/2024_04_05_134610_add_uuid_to_non_akademiks.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\NonAkademik;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Ramsey\Uuid\Uuid;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+
+        Schema::table('non_akademiks', function (Blueprint $table) {
+            $table->uuid('uuid')->index()->after('id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('non_akademiks', function (Blueprint $table) {
+            $table->dropColumn('uuid');
+        });
+    }
+};
