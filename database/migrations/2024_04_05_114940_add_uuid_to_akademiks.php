1e59129e91620a0b09e2e570fe9cd36eece6b95b
arief sangga <arif.sangga@gmail.com>
2024-04-06 23:03:09 +0700

    uuid

--- database/migrations/2024_04_05_114940_add_uuid_to_akademiks.php
+++ database/migrations/2024_04_05_114940_add_uuid_to_akademiks.php
@@ -15,14 +15,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('akademiks', function (Blueprint $table) {
-            $table->uuid('uuid')->after('id')->nullable();
-
-            // Generate UUID for existing records
-            $akademik = Akademik::all();
-            foreach ($akademik as $akademik) {
-                $akademik->uuid = Uuid::uuid4()->toString();
-                $akademik->save();
-            }
+            $table->uuid('uuid')->index()->after('id')->nullable();
         });
     }
 
