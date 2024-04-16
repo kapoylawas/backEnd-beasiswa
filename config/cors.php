a0739bdb7d3e7d92aa4f24c343a2d2d5e776711a
arief sangga <arief.heaven@gmail.com>
2024-04-16 11:52:19 +0700

    update cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => ['http://localhost:5173', 'https://beasiswa-uji.sidoarjokab.go.id', 'https://beasiswa.sidoarjokab.go.id'],
 
     'allowed_origins_patterns' => [],
 
